47f9465e4820b4a1ba8dc01c74f91bbf459f1ade
meow10811 <meow10811+github@gmail.com>
2016-03-29 19:06:12 -0400

    Fixed unitConverter.py to actually run

--- unitConverter.py
+++ unitConverter.py
@@ -1,42 +1,53 @@
 """Has many different functions that convert units.
 
-Includes f_to_c(f), c_to_f(c), f_to_k(f), k_to_f(k), c_to_k(c),
-k_to_c(k).
+Includes .
 """
 
 
-class Solver:
-    """docstring for Solver."""
+class Solve:
+    """docstring for Solve."""
 
-    def __init__(self, find, equation):
+    def __init__(self, input, equation):
         """Define values."""
-        self.find = find
+        input = int(input)
         self.equation = equation
         out = eval(equation)
         self.out = out
 
 
 solverDict = {
-    "Farenheit to Celsius:": "(in - 32) * (5/9)",
-    "Celsius to Farenheit": "(in * (9/5)) + 32",
-    "f_to_k": "(in + 459.67) * (5/9)",
-    "k_to_f": "(in * (9/5)) - 459.67",
-    "c_to_k": "in + 273.15",
-    "k_to_c": "in - 273.15"
+    "Farenheit to Celsius": "(input - 32) * (5/9)",
+    "Celsius to Farenheit": "(input * (9/5)) + 32",
+    "Farenheit to Kelvin": "(input + 459.67) * (5/9)",
+    "Kelvin to Farenheit": "(input * (9/5)) - 459.67",
+    "Celsius to Kelvin": "input + 273.15",
+    "Kelvin to Celsius": "input - 273.15"
 }
-convertOptions = ["Temperature"]
-Temperature1 = ["Farenheit", "Celsius", "Kelvin"]
+
+listDict = {
+    "convertOptions": ["Temperature"],
+    "Temperature": ["Farenheit", "Celsius", "Kelvin"]
+}
 
 
 def poll_user_for_input(message, list):
     """Display a list with a header message, and get input from a user."""
-    print(message)
+    print("\n" + message)
     for i in range(0, len(list)):
         print(str(i + 1) + ". " + str(list[i]))
     return list[int(input("Choose an option: "))-1]
 
+convertTypeChoice = \
+    poll_user_for_input("Category:", listDict["convertOptions"])
 
-input0 = poll_user_for_input("Options:", convertOptions)
-print(input0)
-input1 = poll_user_for_input("H24?", input0+"1")
-print(input1)
+convertFrom = \
+    poll_user_for_input("Type to Convert From:", listDict[convertTypeChoice])
+
+amount = input("How many? ")
+
+convertTo = \
+    poll_user_for_input("Type to Convert To:", listDict[convertTypeChoice])
+
+equation = solverDict[convertFrom + " to " + convertTo]
+answer = Solve(amount, equation).out
+print(str(answer))
